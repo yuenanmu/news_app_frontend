895f5300514b40ab7437556435c3cd2448a41c3d
yuenanmu <1730593502@qq.com>
2026-04-21 17:06:33 +0800

    ``` chore(config): 更新前端应用部署基础路径
    将 GitHub Pages 部署路径从 '/BackEndX/' 更改为 '/news_app_frontend/'
    以匹配正确的项目名称
    ```

--- app_frontend/vite.config.js
+++ app_frontend/vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/BackEndX/', // GitHub Pages 部署路径
+  base: '/news_app_frontend/', // GitHub Pages 部署路径
 })
