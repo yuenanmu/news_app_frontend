af2198545e339e29e79c336c54dcf899bd101993
yuenanmu <1730593502@qq.com>
2026-04-23 15:51:26 +0800

    修改代码，以访问我的云服务器！

--- app_frontend/src/config/api.js
+++ app_frontend/src/config/api.js
@@ -6,7 +6,8 @@
 // API基础URL配置
 export const apiConfig = {
   // 后端API基础URL
-  baseURL: 'http://127.0.0.1:8000',
+  //baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'http://8.136.127.163:8000',
 }
 
 export const aiChatConfig = {
